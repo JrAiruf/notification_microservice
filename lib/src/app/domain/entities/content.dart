2c17dfcfc28559354ea6838a8ad307a4a37e502f
Junior <jos.pd2019@gmail.com>
2023-01-23 12:53:02 -0300

    Notification Entity Done

---
+++ lib/src/app/domain/entities/content.dart
@@ -0,0 +1,8 @@
+import 'dart:js';
+
+class Content {
+  String? _content;
+
+  String? getContent() => _content;
+  void setContent(String value) => _content = value;
+}
--- lib/src/app/domain/entities/notification.dart
+++ lib/src/app/domain/entities/notification.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'notification_properties.dart';
+import 'content.dart';
 
 class Notification {
    String? _notificationId;
@@ -16,3 +16,22 @@ class Notification {
   void setCanceledAt(DateTime value) => _properties?.canceledAt = value;
 
 }
+
+class NotificationProperties {
+  String? recipientId;
+  String? content;
+  String? category;
+  DateTime? readAt;
+  DateTime? canceledAt;
+  DateTime? createdAt;
+
+  NotificationProperties({
+    this.recipientId,
+    this.content,
+    this.category,
+    this.readAt,
+    this.canceledAt,
+    this.createdAt,
+  });
+}
+
